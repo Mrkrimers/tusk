a1e18b17089f74606ccbd15ef4b9c0c508f7b0db
mrkrimers <mrkrimerssasha@gmail.com>
2023-02-03 23:12:33 +0300

    added 021 7

---
+++ 021. metod/7/app.js
@@ -0,0 +1,12 @@
+// 7. На входе задан массив [‘hschool’, ‘hschool_0’, ‘hschool_1’]. Напишите все возможные
+// способы обращения к первому и последнему элементам
+
+const arr = ['hschool', 'hschool_0', 'hschool_1'];
+
+let first = arr[0];
+
+for (let i = 0; i < arr.length; i++) {
+
+console.log(arr[i][0]);
+
+}
