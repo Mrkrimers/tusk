16c2e6cc24d78b2337abfa3a9e35b68109e40010
mrkrimers <mrkrimerssasha@gmail.com>
2023-02-12 22:47:19 +0300

    added 023 all

--- 023.function/1/app.js
+++ 023.function/1/app.js
@@ -1,12 +1,13 @@
 // 1. Пользователь вводит имя фамилию. Необходимо создать функцию
 // возвращающую строку вида «Привет, {имя} {фамилия}»
 
-const name = 'sasha';
-const surname = 'yarmosh';
+const nam = 'sasha';
+const surName = 'yarmosh';
 
-function person(a, b) {
-    return `HI, ${a} ${b}`
+function alex(a, b) {
+    return `Hi! ${nam} ${surName}`
 }
 
-let res = person(name, surname);
-console.log(res);
+let result = alex(nam, surName);
+console.log(result);
+
--- 023.function/2/app.js
+++ 023.function/2/app.js
@@ -3,29 +3,39 @@
 // все четные слова, соответственно, в верхний
 // happy new year -> happy NEW year
 
-const words = 'hschool the best in my Heart'.split(' ');
+
+const str = 'sasha is not a hero';
 
 
-function showResult(words_) {
-    let res = words_.map(function (el, index) {
+// function beta(str_) {
+//     let arr = str_.split(' ');
+//     let NewArr = [];
+//     for (let i = 0; i < arr.length; i++) {
 
-        if (index % 2 == 0) {
-            return el.toLowerCase();
-        } else return el.toUpperCase();
+//         if (i % 2 == 0) {
+//             NewArr.push(arr[i].toLowerCase())
+//         } else NewArr.push(arr[i].toUpperCase())
+
+//     }
+//     return NewArr.join(' ')
+
+// }
+
+// let result = beta(str);
+// console.log(result);
+
+function beta(str_) {
+    const arr = str_.split(' ');
+    let NewArr = arr.map(function (el, index) {
+
+        // if (index % 2 == 0) {
+        //     return el.toLowerCase();
+        // } else return el.toUpperCase();
+        return index % 2 == 0 ? el.toLowerCase() : el.toUpperCase()
 
     })
-    return res;
+    return NewArr.join(' ');
 }
 
-const result = showResult(words);
-
+let result = beta(str);
 console.log(result);
-
-
-
-
-
-
-
-
-
